1da01e90133307539719af2c8fb30ef1a20902b9
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-26 20:50:31 +0530

    added frame_per_sec text into the screen

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,12 +1,15 @@
-from cgitb import reset
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 mp_hand = mp.solutions.hands
 hand = mp_hand.Hands()
 
 mp_draw = mp.solutions.drawing_utils
+
+p_time = 0
+c_time = 0
 
 while True:
     success, img = cap.read()
@@ -26,6 +29,18 @@ while True:
                     )
 
             mp_draw.draw_landmarks(img, hand_lm, mp_hand.HAND_CONNECTIONS)
+
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+
+    cv2.putText(
+        img, str(int(fps)), (10,70),
+        cv2.FONT_HERSHEY_PLAIN, 3,
+        (255,0,255), 5
+    )
+
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
